f049fcbff70ec2981ddd556f345c885dd22dca27
hajjishirwac <shirwachajji@gmail.com>
2022-09-10 14:59:52 +0300

    Introduction to Currying and Partial Application

--- Functional Programming.js
+++ Functional Programming.js
@@ -749,3 +749,17 @@ function urlSlug(title) {
   }
   
   checkPositive([1, 2, 3, -4, 5]);
+
+// // // // // Introduction to Currying and Partial Application
+  function add(x) {
+    // Only change code below this line
+  return function(y) {
+      return function(z) {
+        return x + y + z;
+      };
+    };
+  
+    // Only change code above this line
+  }
+  
+  add(10)(20)(30);
