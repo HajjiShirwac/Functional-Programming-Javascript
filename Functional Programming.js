9a90e7843903365e8c0a2c2eb9398bacc422a6be
hajjishirwac <shirwachajji@gmail.com>
2022-09-10 14:55:47 +0300

    Use the some Method to Check that Any Elements in an Array Meet a Criteria

--- Functional Programming.js
+++ Functional Programming.js
@@ -738,3 +738,14 @@ function urlSlug(title) {
   }
   
   checkPositive([1, 2, 3, -4, 5]);
+
+/// Use the some Method to Check that Any Elements in an Array Meet a Criteria
+
+  function checkPositive(arr) {
+    // Only change code below this line
+  return arr.some(elem => elem > 0);
+    
+    // Only change code above this line
+  }
+  
+  checkPositive([1, 2, 3, -4, 5]);
